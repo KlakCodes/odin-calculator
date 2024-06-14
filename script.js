1d87f8ec33473a741629d1a74f2cae3934769546
KlakCodes <harrisonjclark@hotmail.com>
2024-06-14 15:24:01 +0100

    Created operate function and added comments to code

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let num1;
+let num2;
+let operator;
+
 function add(a, b) {
     return a + b;
 }
@@ -14,7 +18,28 @@ function divide(a, b) {
     return a / b;
 }
 
+function operate (opr, num1, num2) {
+    switch(opr) {
+        case "+":
+            value = add(num1, num2);
+            break;
+        case "-":
+            value = subtract(num1, num2);
+            break;
+        case "*":
+            value = multiply(num1, num2);
+            break;
+        case "/":
+            value = divide(num1, num2);
+            break;
+    }
+
+    return value;
+}
+
 console.log(add(1, 2));
 console.log(subtract(10, 4));
 console.log(multiply(4, 5));
 console.log(divide(40, 5));
+
+console.log(operate("/", 10, 6));
